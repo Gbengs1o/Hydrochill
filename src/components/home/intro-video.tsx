8617ca50fbd8de6b3064a0c1884950e465f797b2
Francis Irewole <firewole8@gmail.com>
2025-09-26 15:19:21 +0000

    wow i love it now please let the audio of the video play as well and add

--- src/components/home/intro-video.tsx
+++ src/components/home/intro-video.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 // YouTube Video ID from the URL: https://youtu.be/zI-raje6Qpc
 const VIDEO_ID = 'zI-raje6Qpc';
@@ -24,11 +25,13 @@ export default function IntroVideo({ onComplete }: IntroVideoProps) {
     // which happens after the fade-out animation (1s) completes.
 
     return () => clearTimeout(fadeTimer);
-  }, []);
+  }, [onComplete]);
 
   const handleAnimationEnd = () => {
     // This function is called when the fade-out animation finishes
-    onComplete();
+    if (isFadingOut) {
+      onComplete();
+    }
   };
 
   return (
@@ -40,12 +43,18 @@ export default function IntroVideo({ onComplete }: IntroVideoProps) {
       onTransitionEnd={handleAnimationEnd}
     >
       <iframe
-        src={`https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&loop=0&modestbranding=1&iv_load_policy=3`}
+        src={`https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&mute=0&controls=0&showinfo=0&rel=0&loop=0&modestbranding=1&iv_load_policy=3&enablejsapi=1`}
         frameBorder="0"
         allow="autoplay; encrypted-media"
         allowFullScreen
         className="w-full h-full"
       ></iframe>
+       <div className="absolute inset-0 flex items-center justify-center bg-black/30 pointer-events-none">
+        <div className="flex flex-col items-center text-white">
+          <Loader2 className="h-12 w-12 animate-spin mb-4" />
+          <p className="text-xl font-semibold tracking-wider">SITE LOADING...</p>
+        </div>
+      </div>
     </div>
   );
 }
